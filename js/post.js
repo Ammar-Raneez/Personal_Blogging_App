2b8409d5b421518087f4d3e1be02a7d6d12b923a
Ammar <ammarraneez@gmail.com>
2021-02-12 11:43:46 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,8 +1,3 @@
-/**
- * TODO (Together): Create getPostIdParam to get the id of the post to use in the request later
- * TODO: Complete getPost function to get post data from API
- * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
- */
 const API_URL = "https://mysterious-escarpment-41897.herokuapp.com/api/models/posts/";
 const API_BASE_URL = "https://mysterious-escarpment-41897.herokuapp.com/";
 
